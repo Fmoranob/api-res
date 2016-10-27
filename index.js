ca41d97690e40f08e6e91e58635500081c86cf1a
Scherzo <concerto@ishtera.net>
2016-10-27 15:04:26 -0300

    Version 0.3
    Agregados parametros REST y repsuestas

--- index.js
+++ index.js
@@ -9,8 +9,32 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/hola/:name', (req, res) => {
-	res.send({ message: `Hola ${req.params.name}`})
+app.get('/api/product', (req, res) => {
+	res.send(200, {products: []})
+})
+
+app.get('/api/product/:productId', (req, res) => {
+	let productId = req.params.productoId
+
+	Product.findById(productId, (err, product) {
+		if (err) return res.status(500).send({message: `Error al realizar la pretición: ${err}`})
+		if (!product) return res.status(404).send({message: 'El producto no existe'})
+
+		res.status(200).send({ product })
+	})
+})
+
+app.post('/api/product', (req, res) => {
+	console.log(req.body)
+	res.status(200).send({message: 'El producto se ha recibido'})
+})
+
+app.put('/api/product/:productId', (req, res) => {
+
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+
 })
 
 app.listen(port, () => {
